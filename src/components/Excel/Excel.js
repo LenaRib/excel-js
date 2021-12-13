e8a833ad2507bba5c2ed8de2c5e788abd7538524
LenaRib <poddubnayaeo@gmail.com>
2021-12-13 13:34:37 +0100

    class for DOM

--- src/components/Excel/Excel.js
+++ src/components/Excel/Excel.js
@@ -2,7 +2,7 @@ import {$} from '@core/dom'
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
   }
   getRoot() {
@@ -11,18 +11,13 @@ export class Excel {
     this.components.forEach(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     });
 
     return $root;
   }
   render() {
-    // console.log(this.$el);
-    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
-    // const node = document.createElement('h1');
-    // node.textContent = 'Test';
     this.$el.append(this.getRoot())
-    console.log(this.components);
   }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -2,7 +2,6 @@ const newLocal = 'No root provided for DomListener!'
 
 export class DomListener {
   constructor($root) {
-    console.log('что пришло ', $root)
     if (!$root) {
       throw new Error(newLocal)
     }
