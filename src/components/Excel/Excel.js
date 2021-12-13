babcfc58f1373b53cef764ace084103b8700ae7f
LenaRib <poddubnayaeo@gmail.com>
2021-12-13 09:43:10 +0100

    add render

--- src/components/Excel/Excel.js
+++ src/components/Excel/Excel.js
@@ -3,4 +3,22 @@ export class Excel {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
+  getRoot() {
+    const $root = document.createElement('div');
+    // $root.textContent = 'root Test'
+    // $root.style.fontSize = '10rem'
+    this.components.forEach(Component => {
+      const component = new Component()
+      $root.insertAdjacentHTML('beforeend', component.toHTML())
+    });
+    return $root;
+  }
+  render() {
+    // console.log(this.$el);
+    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
+    // const node = document.createElement('h1');
+    // node.textContent = 'Test';
+    this.$el.append(this.getRoot())
+    console.log(this.components);
+  }
 }
