3e636f577227bfc16c56b49b3bdfe6500c175494
LenaRib <poddubnayaeo@gmail.com>
2021-12-13 11:38:16 +0100

    dom class

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,2 +1,11 @@
+const newLocal = 'No root provided for DomListener!'
+
 export class DomListener {
+  constructor($root) {
+    console.log('что пришло ', $root)
+    if (!$root) {
+      throw new Error(newLocal)
+    }
+    this.$root = $root
+  }
 }
