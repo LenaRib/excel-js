51f00940d6959acdf5698658e2f54c18b1735c7f
LenaRib <poddubnayaeo@gmail.com>
2021-12-13 18:04:36 +0100

    stracture for listeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,10 +1,19 @@
 const newLocal = 'No root provided for DomListener!'
 
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error(newLocal)
     }
     this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    console.log(this.listeners)
+  }
+
+  deleteDOMListernes() {
+
   }
 }
