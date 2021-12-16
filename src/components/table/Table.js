c31ee006dcfad35363eae73cc6297e94b10460ea
LenaRib <poddubnayaeo@gmail.com>
2021-12-16 22:20:18 +0100

    finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,13 +1,14 @@
-import {ExcelComponent} from '../../core/ExcelComponent';
-import {createTable} from './table.template';
-import {$} from '@core/dom';
+import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/table/table.template'
+import {resizeHandler} from './table.resize'
+import {shouldResize} from './table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown', 'mouseup'],
+      listeners: ['mousedown'],
     })
   }
 
@@ -15,34 +16,9 @@ export class Table extends ExcelComponent {
     return createTable(20)
   }
 
-
   onMousedown(event) {
-    console.log('this ', this)
-    if (event.target.dataset.resize) {
-      // console.log('Start resizing', event.target)
-      // this.cell.push(event.target.dataset.resize)
-      const $resizer = $(event.target)
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      // console.log($parent.getCoords())
-      const cords = $parent.getCoords()
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-
-      document.onmousemove = e => {
-        const delta = e.pageX - cords.right
-        const value = cords.width + delta
-        $parent.$el.style.width = value + 'px'
-        cells
-            .forEach(el => {
-              el.style.width = value + 'px'
-            })
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null
-      }
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
-  }
-  onMouseup(event) {
-    console.log('Stop resizing')
   }
 }
